4b736f54276e43508acccfe33602ef8f45079159
norwegJan <236618355+norwegJan@users.noreply.github.com>
2026-04-21 21:54:29 +0200

    base/mobile-layout for all containers more or less done

---
+++ script.js
@@ -0,0 +1,13 @@
+const dmToggle = document.querySelector("#dm-toggle");
+
+const answerInputs = document.querySelectorAll(".answer-input");
+
+dmToggle.addEventListener("change", (e) => {
+  console.log(dmToggle.checked);
+});
+
+answerInputs.forEach((input) => {
+  input.addEventListener("change", (e) => {
+    console.log(e.target.value);
+  });
+});
