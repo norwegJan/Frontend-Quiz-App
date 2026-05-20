cf67058082d81225ad660431f09740324681dced
norwegJan <236618355+norwegJan@users.noreply.github.com>
2026-05-20 16:48:06 +0200

    improved darkmode-toggle using design tokens and added localStorage-functionality

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
 // DECLARE VARIABLES
 
 const dmToggle = document.querySelector("#dm-toggle");
-const bodyEl = document.querySelector("#body");
-const dmEls = document.querySelectorAll(".dm-el");
-const outOfEl = document.querySelector("#out-of-el");
+const darkMode = localStorage.getItem("darkMode");
+
+const htmlBtn = document.querySelector("#html-btn");
+const cssBtn = document.querySelector("#css-btn");
+const jsBtn = document.querySelector("#js-btn");
+const a11yBtn = document.querySelector("#a11y-btn");
 
 const answerInputs = document.querySelectorAll(".answer-input");
+
+const submitBtn = document.querySelector("#submit-btn");
+const againBtn = document.querySelector("#again-btn");
 
 // ADD EVENT LISTENERS
 
 dmToggle.addEventListener("change", (e) => {
-  if (!dmToggle.checked) {
-    bodyEl.classList.remove("darkmode");
-    dmEls.forEach((el) => el.classList.remove("darkmode-active"));
-    outOfEl.classList.remove("darkmode-out-of");
+  document.body.classList.toggle("darkmode", dmToggle.checked);
+  if (dmToggle.checked) {
+    localStorage.setItem("darkMode", "enabled");
   } else {
-    bodyEl.classList.add("darkmode");
-    dmEls.forEach((el) => el.classList.add("darkmode-active"));
-    outOfEl.classList.add("darkmode-out-of");
+    localStorage.removeItem("darkMode");
   }
 });
 
-/* answerInputs.forEach((input) => {
+if (darkMode === "enabled") {
+  dmToggle.checked = true;
+  document.body.classList.add("darkmode");
+}
+
+htmlBtn.addEventListener("click", () => {
+  console.log("HTML-Button clicked");
+});
+
+cssBtn.addEventListener("click", () => {
+  console.log("CSS-Button clicked");
+});
+
+jsBtn.addEventListener("click", () => {
+  console.log("JS-Button clicked");
+});
+
+a11yBtn.addEventListener("click", () => {
+  console.log("A11Y-Button clicked");
+});
+
+answerInputs.forEach((input) => {
   input.addEventListener("change", (e) => {
     console.log(e.target.value);
   });
-}); */
+});
+
+submitBtn.addEventListener("click", () => {
+  console.log("Submit-Button clicked");
+});
+
+againBtn.addEventListener("click", () => {
+  console.log("again-Button clicked");
+});
 
 // DECLARE FUNCTIONS
